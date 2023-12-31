c5cf2e15d852edb8b57d272204a0d42e1b34c316
brianonchain <jonwayhuang@gmail.com>
2023-12-31 22:16:32 +0800

    make it better

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.telegram.setWebhook(process.env.URL);
-bot.startWebhook("/", null, 5000); // can't be same port as listening
+bot.startWebhook("/", null, process.env.PORT); // can't be same port as listening
 
 bot.command("start", (ctx) => {
   bot.telegram.sendMessage(ctx.chat.id, "Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it", {});
@@ -26,15 +26,15 @@ bot.help((ctx) => {
 
 // bot.launch();
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(bot.webhookCallback("/"));
+// app.use(express.json());
+// app.use(bot.webhookCallback("/"));
 
-// app.get("/", (req, res) => {
-//   res.sendStatus(200);
+// // app.get("/", (req, res) => {
+// //   res.sendStatus(200);
+// // });
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`listening on port ${process.env.PORT}`);
 // });
-
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
